8a3ca2c464657b3b4bbc357e7dac6cc5191dc3d3
zachery estefano <zacheryestefano@gmail.com>
2022-01-19 18:10:26 -0500

    signup route working

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,35 @@
-const express = require('express')
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
+
+
+
+// Sign up
+router.post(
+    '/',
+    asyncHandler(async (req, res) => {
+      const { email, firstName, lastName, password, username } = req.body;
+      const user = await User.signup({ email, firstName, lastName, username, password });
+  
+      await setTokenCookie(res, user);
+  
+      return res.json({
+        user
+      });
+    })
+  );
+
+//   nuqpCG2R-Zbz2w1TSsGf5yHBX63dpScbuWBQ
+
+
+
+
+
+
 
 
 

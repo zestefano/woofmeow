1619ef78d446a19df2258c64299ad64c0a9e3b3b
zachery estefano <zacheryestefano@gmail.com>
2022-01-24 18:15:04 -0500

    get Reviews working

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,12 +2,15 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const sitterRouter = require('./sitters.js')
+const reviewRouter = require('./reviews.js')
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
 router.use('/sitters', sitterRouter)
+
+router.use('/reviews', reviewRouter)
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });

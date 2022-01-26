611b97164f4d6d0d7547b9de2dce327cd1ea251b
zachery estefano <zacheryestefano@gmail.com>
2022-01-26 17:08:14 -0500

    heroku test

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 
 
